97b0081d67cc33595ac50acd6da3e21b476e036f
I`am Caliph <92326690+Caliph91@users.noreply.github.com>
2021-10-18 13:30:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-pkg install ffmpeg -y
-pkg install libwebp -y
-pkg install imagemagick -y
-pkg install nodejs -y
-pkg install tesseract -y
+apt-get install ffmpeg -y
+apt-get install libwebp -y
+apt-get install imagemagick -y
+apt-get install nodejs -y
+apt-get install tesseract -y
 npm install
