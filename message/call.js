5c7255867a4e938bc4d2dc39015c92adc7e5f9cc
I`am Caliph <92326690+Caliph91@users.noreply.github.com>
2021-10-18 18:39:16 +0700

    Iyah

--- message/call.js
+++ message/call.js
@@ -1,6 +1,8 @@
 async function a(conn, json) {
 let { from } = json[2][0][1]
     let id = json[2][0][2][0][1]["call-id"]
+    let { name } = conn.contacts[id.split('@')[0] + '@s.whatsapp.net']
+    if (name) return
    await conn.rejectIncomingCall(from, id)
    await conn.sendMessage(from, '「 Reject Call 」\nMaaf Kami Tidak Bisa Menerima Panggilan!\n\nSorry We Can\'t Receive Calls!\n\nTelpon/call = block', 'conversation')
   // conn.blockUser(from)
